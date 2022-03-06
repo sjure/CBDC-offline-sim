a53c29152ed6d847ab9be90fcc6aeec2c2c78c51
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-06 16:32:10 +0100

    add bfs search only passing network nodes

---
+++ src/modules/bfs.py
@@ -0,0 +1,28 @@
+from queue import Queue
+from modules.types import NETWORK, INTERMEDIARY
+
+def BFS_to_intermediary(nodes, start_node):
+    visited = set()
+    queue = Queue()
+
+    queue.put(start_node)
+    visited.add(start_node)
+    
+    path_found = False
+    intermediary_node = -1
+
+    while not queue.empty():
+        current_node_id = queue.get()
+        current_node_data = nodes[current_node_id]["data"]
+
+        if (current_node_data.type == INTERMEDIARY and current_node_data.is_online):
+            path_found = True
+            intermediary_node = current_node_data
+            break
+        elif ((current_node_data.type == NETWORK and current_node_data.is_online) or current_node_id == start_node):
+            for next_node in current_node_data.network_neighbors:
+                if next_node not in visited:
+                    queue.put(next_node)
+                    visited.add(next_node)            
+
+    return (path_found, current_node_data)
