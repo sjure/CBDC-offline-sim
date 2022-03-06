aedf490b5a20875416d641b84ee5b47986671557
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-06 16:42:35 +0100

    add request money to request transaction from intermediary

--- src/modules/user_node.py
+++ src/modules/user_node.py
@@ -2,22 +2,31 @@ from modules.node import Node
 from modules.types import USER
 import secrets
 from numpy import random
+from modules.bfs import BFS_to_intermediary
 
 class UserNode(Node):
     transactions = dict()
     type = USER
-    def __init__(self, bc=None, sim=None, tx_rate=0.1, node_id=-1):
-        self.bc = bc
+    def __init__(self, sim=None, tx_rate=0.1, node_id=-1,**attr):
+        super().__init__(node_id=node_id, **attr)
         self.sim = sim
         self.tx_rate = tx_rate
         self.node_id = node_id
-        self.id = secrets.token_hex(16)
 
     def request_money(self):
-        # Get closest intermediary only transiting routers
-        # Get nearby user to give money
+        has_connection_to_intermediary, intermediary = self.get_closest_intermediary()
+        if (has_connection_to_intermediary):
+            target = self.neighbors[random.randint(0, len(self.neighbors))]
+            amount = random.randint(1, 100)
+            intermediary.request_transaction(self.node_id, target, amount)
+            # Get nearby user to give money
 
-        pass
+    def get_closest_intermediary(self):
+        return BFS_to_intermediary(self.graph.nodes, self.node_id)
+
+    
+    def approve_transaction(self,from_node_id, amount):
+        return True
     
     def do_transaction(self):
         # Get closest intermediary only transiting routers
@@ -25,13 +34,13 @@ class UserNode(Node):
         # Send money, amount = 
 
         #print(f"transaction {self.id}")
-        pass
+        self.request_money()
 
     def get_balance(self):
-        return self.bc.balance_of(self.id)
+        return self.bc.balance_of(self.account_id)
 
     def tick(self):
-        if (random.random() >= self.tx_rate):
+        if (random.random() <= self.tx_rate):
             self.sim.add_event(self.do_transaction)
 
     def remove_from_balance(self):
@@ -39,4 +48,8 @@ class UserNode(Node):
 
     def add_to_balance(self):
         pass
+
+    def approve_transaction(self, to, amount):
+        # Some algorithm for determining if node wants to pay
+        return True
     
