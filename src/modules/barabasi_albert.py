5a18567fcf8894199f0aab29f25045b5447d8039
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-06 16:38:27 +0100

    add intermediary and network nodes to the network

--- src/modules/barabasi_albert.py
+++ src/modules/barabasi_albert.py
@@ -1,14 +1,22 @@
 from modules.graph import Graph, nx, plt
 from modules.user_node import UserNode
-
+from modules.network_node import NetworkNode
+from modules.intermediary_node import IntermediaryNode
 
 class BarabasiAlbert(Graph):
     def __init__(self, n=100, m=3,**attr):
         super().__init__(**attr)
         g = nx.barabasi_albert_graph(n, m, seed=self.seed)
-        new_nodes = [(i,dict(data=UserNode(bc=self.bc, node_id=i, sim=self.sim,tx_rate=attr.get("tx_rate")))) for i in g.nodes]
+        new_nodes = [(i,dict(data=UserNode(node_id=i, sim=self.sim,tx_rate=attr.get("tx_rate"),graph=self))) for i in g.nodes]
         self.add_nodes_from(new_nodes)
         self.add_edges_from(g.edges)
+        network_nodes = [(1000,dict(data=NetworkNode(node_id=1000,graph=self)))]
+        self.add_edges_from([(i,1000) for i in list(self.nodes)])
+        self.add_nodes_from(network_nodes)
+        intermediary_nodes = [(2000,dict(data=IntermediaryNode(node_id=2000,graph=self,bc=self.bc)))]
+        self.add_nodes_from(intermediary_nodes)
+        self.add_edges_from([(2000,1000)])
+        self.init_neighbors()
 
 
 if __name__ == '__main__':
