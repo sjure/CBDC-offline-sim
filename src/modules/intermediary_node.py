904bcd36e368066041d5002287b6dce5cbc8e9dc
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-09 11:49:59 +0100

    add docstrings for class and file

--- src/modules/intermediary_node.py
+++ src/modules/intermediary_node.py
@@ -1,29 +1,28 @@
+""" The intermediary Node """
 from modules.node import Node
 from modules.types import INTERMEDIARY
 
+
 class IntermediaryNode(Node):
-        is_online = True
-        type = INTERMEDIARY
+    """ Intermediary Node processes the blockchain """
+    is_online = True
+    type = INTERMEDIARY
 
-        def __init__(self, bc=None, sim=None, node_id=-1,**attr):
-            super().__init__(node_id=node_id, **attr)
-            self.bc = bc
+    def __init__(self, bc=None, sim=None, node_id=-1,**attr):
+        super().__init__(node_id=node_id, **attr)
+        self.bc = bc
 
-        def request_transaction(self, payee_node_id, payer_node_id, amount):
-            payer_node = self.graph.get_node(payer_node_id)
-            payee_node = self.graph.get_node(payee_node_id)
-            if not payer_node.approve_transaction(payee_node_id, amount):
-                return False
-            if (self.get_funds_of_node(payer_node.account_id) <= amount):
-                return False
-            self.bc.add_transaction(payee_node.account_id, payer_node.account_id, amount)
+    def request_transaction(self, payee_node_id, payer_node_id, amount):
+        payer_node = self.graph.get_node(payer_node_id)
+        payee_node = self.graph.get_node(payee_node_id)
+        if not payer_node.approve_transaction(payee_node_id, amount):
+            return False
+        if (self.get_funds_of_node(payer_node.account_id) <= amount):
+            return False
+        self.bc.add_transaction(payee_node.account_id, payer_node.account_id, amount)
 
-        def get_funds_of_node(self, account_id):
-            return self.bc.balance_of(account_id)
-        
-        def tick(self):
-            pass
-        
-
-
-        
+    def get_funds_of_node(self, account_id):
+        return self.bc.balance_of(account_id)
+    
+    def tick(self):
+        pass
