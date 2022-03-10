c5fc9e16e189cf316211b8cd0d9183d79265dfd9
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-10 12:50:07 +0100

    add statistics class with counters

---
+++ src/Statistics.py
@@ -0,0 +1,15 @@
+from Config import InputsConfig as p
+
+
+class Statistics:
+    online_tx_amount = 0
+    online_tx_volume = 0
+    offline_tx_amount = 0
+    offline_tx_volume = 0
+    network_failures = 0
+    intermediary_failures = 0
+    fradulent_tx_successes = 0
+    fradulent_tx_failures = 0
+    fradulent_tx_success_volume = 0
+    fradulent_tx_failures_volume = 0
+
