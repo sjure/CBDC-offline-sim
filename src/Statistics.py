ca4872bc51e966b63ef53c86cdf221d484b354be
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-10 14:34:09 +0100

    add print state functions

--- src/Statistics.py
+++ src/Statistics.py
@@ -1,5 +1,7 @@
 from Config import InputsConfig as p
 
+def str_ljust(data_point :int):
+    return str(data_point).ljust(20)
 
 class Statistics:
     # TX data
@@ -17,3 +19,26 @@ class Statistics:
     # HW failures
     network_failures = 0
     intermediary_failures = 0
+
+    def print_offline_online():
+        print("Online".ljust(40) + "Offline".ljust(40))
+        print("Tx".ljust(20) + "Volume".ljust(20) + "Tx".ljust(20) + "Volume".ljust(20))
+        print(str_ljust(Statistics.online_tx) + str_ljust(Statistics.online_tx_volume) + 
+            str_ljust(Statistics.offline_tx) + str_ljust(Statistics.offline_tx_volume))
+
+    def print_fraud():
+        print("Fraud Success".ljust(40) + "Fraud Failure".ljust(40))
+        print("Tx".ljust(20) + "Volume".ljust(20) + "Tx".ljust(20) + "Volume".ljust(20))
+        print(str_ljust(Statistics.fradulent_tx_successes) + str_ljust(Statistics.fradulent_tx_success_volume) + 
+            str_ljust(Statistics.fradulent_tx_failures) + str_ljust(Statistics.fradulent_tx_failures_volume))
+
+    def print_state():
+        Statistics.print_offline_online()
+        print()
+        Statistics.print_fraud()
+        
+
+
+
+if __name__ == "__main__":
+    Statistics.print_state()
