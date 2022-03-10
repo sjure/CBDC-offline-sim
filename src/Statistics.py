61b4aa8ca0664c9a3d74a0a9309ae0ce0952ab18
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-10 15:05:09 +0100

    add print of money supply

--- src/Statistics.py
+++ src/Statistics.py
@@ -1,7 +1,7 @@
 from Config import InputsConfig as p
 
-def str_ljust(data_point :int):
-    return str(data_point).ljust(20)
+def str_ljust(data_point :int,spacing = 20):
+    return str(data_point).ljust(spacing)
 
 class Statistics:
     # TX data
@@ -42,10 +42,21 @@ class Statistics:
         print(str_ljust(Statistics.fradulent_tx_successes) + str_ljust(Statistics.fradulent_tx_success_volume) + 
             str_ljust(Statistics.fradulent_tx_failures) + str_ljust(Statistics.fradulent_tx_failures_volume))
 
+
+    def print_money_supply():
+        print("".ljust(20) + "Value Before".ljust(10) + "Value After ".ljust(10))
+        print("Online".ljust(20) + str_ljust(Statistics.online_money_init,spacing=10) + str_ljust(Statistics.online_money_after,spacing=10))
+        print("Offline".ljust(20) + str_ljust(Statistics.offline_money_init,spacing=10) + str_ljust(Statistics.offline_money_after,spacing=10))
+        print("Sum".ljust(20) + str_ljust(Statistics.total_money_before,spacing=10) + str_ljust(Statistics.total_money_after,spacing=10))
+
+
+
     def print_state():
         Statistics.print_offline_online()
         print()
         Statistics.print_fraud()
+        print()
+        Statistics.print_money_supply()
         
 
 
