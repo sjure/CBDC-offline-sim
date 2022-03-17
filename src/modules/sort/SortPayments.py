93fd4ec00cc541201f4e8fff3c236a5c9acd786c
Sjur Brekke Espedal <sjur.b.espedal@gmail.com>
2022-03-17 17:38:47 +0100

    add topology sort for payments

---
+++ src/modules/sort/SortPayments.py
@@ -0,0 +1,28 @@
+
+
+def topologicalSort(payment_number,visited_payments,stack, payment_address_map, payments):
+    visited_payments[payment_number] = True
+
+    for i in payment_address_map[payments[payment_number].tx.from_address]:
+        if not visited_payments[i]:
+            topologicalSort(i, visited_payments,stack, payment_address_map, payments)
+    
+    stack.append(payments[i])
+
+
+
+def sort_payments(payments):
+    visited_payments = [False]*len(payments)
+    stack = []
+    payment_address_map = {}
+    for payment_number, payment in enumerate(payments):
+        if payment.tx.from_address in payment_address_map.keys():
+            payment_address_map[payment.tx.from_address].append(payment_number)
+        else:
+            payment_address_map[payment.tx.from_address] = [payment_number]
+
+    for i in range(len(payments)):
+        if not visited_payments[i]:
+            topologicalSort(i,visited_payments,stack, payment_address_map, payments)
+    return stack[::-1]
+
